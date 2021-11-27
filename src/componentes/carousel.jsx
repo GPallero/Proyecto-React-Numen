3d421b42e8b3a4dade46a01c3a7dc1a2ffdadfb0
Constanza <constanza.ludu@gmail.com>
2021-11-27 19:35:33 -0300

    Le agregue algunos estilos y optimice la transicion y el efecto del carousel

--- src/componentes/carousel.jsx
+++ src/componentes/carousel.jsx
@@ -12,22 +12,27 @@ import SalinasGrandes from "../images/salinas-grandes.png";
 import SanJuan from "../images/san-juan.jpg";
 import TierraDelFuego from "../images/tierra-fuego.jpg";
 import Caminito from "../images/caminito.jpg";
+  
 
 
 const MiCarousel = () => {
   return (
     <>
-    <div className="slides-container">
-      <div className="slides-items">
-        <div><img src={Iguazu} alt="First slide"/></div>
-        <div><img src={Jujuy} alt="Second slide"/></div>
-        <div><img src={Glaciar} alt="Third slide"/></div>
-        <div><img src={BA} alt="Forth slide"/></div>
-        <div><img src={Llamas} alt="Fifth slide"/></div>
-        <div><img src={Iguazu} alt="First slide"/></div>
-        <div><img src={Jujuy} alt="Second slide"/></div>
-     </div>
-    </div>
+    <section>
+      <h3>Follow us on Instagram</h3>
+      <h2>@TurismoVueLorem</h2>
+      <div className="slides-container">
+        <div className="slides-items">
+          <div className='slides-items-div'><img src={Caminito} alt="First slide"/></div>
+          <div className='slides-items-div'><img src={Jujuy} alt="Second slide"/></div>
+          <div className='slides-items-div'><img src={Glaciar} alt="Third slide"/></div>
+          <div className='slides-items-div'><img src={BA} alt="Forth slide"/></div>
+          <div className='slides-items-div'><img src={Iguazu} alt="Fifth slide"/></div>
+          <div className='slides-items-div'><img src={Caminito} alt="First slide"/></div>
+          <div className='slides-items-div'><img src={Jujuy} alt="Second slide"/></div>
+        </div>
+      </div>
+    </section>
     </>
   );
 };
--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import { NavDropdown, Nav } from "react-bootstrap";
+import { NavDropdown, Nav, Button, Form, FormControl } from "react-bootstrap";
+import logo from '../images/logo.jpeg'
 
 const Navegacion = () => {
     return (
-        <>
-       <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-</>
+      <>
+        <Navbar bg="light" expand="lg" fixed="top">
+          <Container>
+                <Navbar.Brand href="#home">
+                <img
+                  alt=""
+                  src={logo}
+                  width="80"
+                  height="auto"
+                  className="d-inline-block align-center"
+                />{' '}
+              </Navbar.Brand>
+            <Navbar.Brand href="#home">VueLorem</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>
       );
 }
  
