7723793f4156da4461ab3c444e5f98531b01ea2d
Grupo 2 <gpallero88@gmail.com>
2021-11-29 19:02:45 -0300

    Agregada: AlertBar y NavBar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import './App.css';
-// import Header from './components/Header/Header';
+import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-// import Footer from './components/Footer/Footer';
-
+import Footer from './components/Footer/Footer';
 
 
 function App() {
   return (
     <div>
-       {/* <Header/> */}
+       <Header/>
        <Main/>
-       {/* <Footer/> */}
+       <Footer/>
    </div>
-  );
+  )
 }
 
-export default App;
+export default App
